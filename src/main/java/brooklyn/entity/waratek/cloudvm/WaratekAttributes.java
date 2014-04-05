830ba2fb041cd79a0e163151393b0774167ae801
Andrew Kennedy <andrew.kennedy@cloudsoftcorp.com>
2014-04-05 20:58:36 +0100

    Convert WaratekAttributes to class with rendering hints for display

--- src/main/java/brooklyn/entity/waratek/cloudvm/WaratekAttributes.java
+++ src/main/java/brooklyn/entity/waratek/cloudvm/WaratekAttributes.java
@@ -15,34 +15,61 @@
  */
 package brooklyn.entity.waratek.cloudvm;
 
+import java.util.concurrent.atomic.AtomicBoolean;
+
+import brooklyn.config.render.RendererHints;
 import brooklyn.event.AttributeSensor;
 import brooklyn.event.basic.Sensors;
+import brooklyn.util.text.ByteSizeStrings;
 
-public interface WaratekAttributes  {
+public class WaratekAttributes  {
 
     /*
      * JVC attributes available through the waratek MXBeans.
      */
 
-    AttributeSensor<Long> BYTES_SENT = Sensors.newLongSensor("waratek.jvc.bytesSent", "Total network bytes sent");
-    AttributeSensor<Long> BYTES_RECEIVED = Sensors.newLongSensor("waratek.jvc.bytesReceived", "Total network bytes received");
-    AttributeSensor<Integer> FILE_DESCRIPTOR_COUNT = Sensors.newIntegerSensor("waratek.jvc.fileDescriptorCount", "Current open file descriptors");
-    AttributeSensor<Double> CPU_USAGE = Sensors.newDoubleSensor("waratek.jvc.cpuUsage", "Current CPU usage");
-    AttributeSensor<String> STATUS = Sensors.newStringSensor("waratek.jvc.status", "Current JVC status");
+    public static final AttributeSensor<Long> BYTES_SENT = Sensors.newLongSensor("waratek.jvc.bytesSent", "Total network bytes sent");
+    public static final AttributeSensor<Long> BYTES_RECEIVED = Sensors.newLongSensor("waratek.jvc.bytesReceived", "Total network bytes received");
+    public static final AttributeSensor<Integer> FILE_DESCRIPTOR_COUNT = Sensors.newIntegerSensor("waratek.jvc.fileDescriptorCount", "Current open file descriptors");
+    public static final AttributeSensor<Double> CPU_USAGE = Sensors.newDoubleSensor("waratek.jvc.cpuUsage", "Current CPU usage");
+    public static final AttributeSensor<String> STATUS = Sensors.newStringSensor("waratek.jvc.status", "Current JVC status");
 
     /*
      * Aggregate sensor attributes accumulated from the JVC clusters.
      */
 
-    AttributeSensor<Long> TOTAL_HEAP_MEMORY = Sensors.newLongSensor("waratek.heapMemory.total", "Total aggregated heap memory usage");
-    AttributeSensor<Double> HEAP_MEMORY_DELTA_PER_SECOND_LAST = Sensors.newDoubleSensor("waratek.heapMemoryDelta.last", "Change in heap memory usage per second");
-    AttributeSensor<Double> HEAP_MEMORY_DELTA_PER_SECOND_IN_WINDOW = Sensors.newDoubleSensor("waratek.heapMemoryDelta.windowed", "Average change in heap memory usage over 30s");
-    AttributeSensor<Double> AVERAGE_CPU_USAGE = Sensors.newDoubleSensor("waratek.cpuUsage.average", "Average CPU usage across the cluster");
+    public static final AttributeSensor<Long> TOTAL_HEAP_MEMORY = Sensors.newLongSensor("waratek.heapMemory.total", "Total aggregated heap memory usage");
+    public static final AttributeSensor<Double> HEAP_MEMORY_DELTA_PER_SECOND_LAST = Sensors.newDoubleSensor("waratek.heapMemoryDelta.last", "Change in heap memory usage per second");
+    public static final AttributeSensor<Double> HEAP_MEMORY_DELTA_PER_SECOND_IN_WINDOW = Sensors.newDoubleSensor("waratek.heapMemoryDelta.windowed", "Average change in heap memory usage over 30s");
+    public static final AttributeSensor<Double> AVERAGE_CPU_USAGE = Sensors.newDoubleSensor("waratek.cpuUsage.average", "Average CPU usage across the cluster");
 
     /*
      * Counter attributes.
      */
 
-    AttributeSensor<Integer> JVM_COUNT = Sensors.newIntegerSensor("waratek.jvmCount", "Number of JVMs");
-    AttributeSensor<Integer> JVC_COUNT = Sensors.newIntegerSensor("waratek.jvcCount", "Number of JVCs");
+    public static final AttributeSensor<Integer> JVM_COUNT = Sensors.newIntegerSensor("waratek.jvmCount", "Number of JVMs");
+    public static final AttributeSensor<Integer> JVC_COUNT = Sensors.newIntegerSensor("waratek.jvcCount", "Number of JVCs");
+
+    private static AtomicBoolean initialized = new AtomicBoolean(false);
+
+    /** Setup renderer hints for the MXBean attributes. */
+    @SuppressWarnings("rawtypes")
+    public static void init() {
+        if (initialized.get()) return;
+        synchronized (initialized) {
+            if (initialized.get()) return;
+
+            RendererHints.register(TOTAL_HEAP_MEMORY, new RendererHints.DisplayValue(ByteSizeStrings.metric()));
+            RendererHints.register(HEAP_MEMORY_DELTA_PER_SECOND_LAST, new RendererHints.DisplayValue(ByteSizeStrings.metric()));
+            RendererHints.register(HEAP_MEMORY_DELTA_PER_SECOND_IN_WINDOW, new RendererHints.DisplayValue(ByteSizeStrings.metric()));
+            RendererHints.register(BYTES_SENT, new RendererHints.DisplayValue(ByteSizeStrings.metric()));
+            RendererHints.register(BYTES_RECEIVED, new RendererHints.DisplayValue(ByteSizeStrings.metric()));
+
+            initialized.set(true);
+        }
+    }
+
+    static {
+        init();
+    }
 }
