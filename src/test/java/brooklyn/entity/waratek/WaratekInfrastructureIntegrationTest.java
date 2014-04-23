6852da3708d4f9167a105b962eb3043e17907be8
Andrew Kennedy <andrew.kennedy@cloudsoftcorp.com>
2014-04-23 12:05:15 +0100

    Adding initial integration test

---
+++ src/test/java/brooklyn/entity/waratek/WaratekInfrastructureIntegrationTest.java
@@ -0,0 +1,66 @@
+/*
+ * Copyright 2014 by Cloudsoft Corporation Limited
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package brooklyn.entity.waratek;
+
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import brooklyn.entity.BrooklynAppLiveTestSupport;
+import brooklyn.entity.basic.Entities;
+import brooklyn.entity.proxying.EntitySpec;
+import brooklyn.entity.trait.Startable;
+import brooklyn.entity.waratek.cloudvm.WaratekInfrastructure;
+import brooklyn.location.Location;
+import brooklyn.location.LocationSpec;
+import brooklyn.location.basic.FixedListMachineProvisioningLocation;
+import brooklyn.test.EntityTestUtils;
+
+import com.google.common.collect.ImmutableList;
+
+/**
+ * Waratek integration tests.
+ */
+public class WaratekInfrastructureIntegrationTest extends BrooklynAppLiveTestSupport {
+
+    protected Location testLocation;
+    protected WaratekInfrastructure infrastructure;
+
+    @BeforeMethod(alwaysRun = true)
+    @Override
+    public void setUp() throws Exception {
+        super.setUp();
+        testLocation = mgmt.getLocationManager().createLocation(LocationSpec.create(FixedListMachineProvisioningLocation.class)
+                .configure("machines", "192.168.2.149"));
+    }
+
+    /**
+     * Test that a node starts and sets SERVICE_UP correctly.
+     */
+    @Test(groups = "Integration")
+    public void canStartupAndShutdown() {
+        infrastructure = app.createAndManageChild(EntitySpec.create(WaratekInfrastructure.class)
+                .configure("initialSize", "1")
+                .configure("", "test-infrastructure"));
+        app.start(ImmutableList.of(testLocation));
+
+        EntityTestUtils.assertAttributeEqualsEventually(infrastructure, Startable.SERVICE_UP, true);
+        Entities.dumpInfo(app);
+
+        infrastructure.stop();
+
+        EntityTestUtils.assertAttributeEqualsEventually(infrastructure, Startable.SERVICE_UP, false);
+    }
+}
