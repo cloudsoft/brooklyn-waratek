7eae732d11ce4e2298fd4ade6e7f04bba739fc2d
Andrew Kennedy <andrew.kennedy@cloudsoftcorp.com>
2014-04-23 11:59:34 +0100

    Some initial changes for elastic memory in the waratek application group

--- src/main/java/brooklyn/entity/waratek/cloudvm/WaratekApplicationGroup.java
+++ src/main/java/brooklyn/entity/waratek/cloudvm/WaratekApplicationGroup.java
@@ -15,13 +15,16 @@
  */
 package brooklyn.entity.waratek.cloudvm;
 
+import brooklyn.entity.Application;
 import brooklyn.entity.annotation.Effector;
 import brooklyn.entity.annotation.EffectorParam;
 import brooklyn.entity.basic.BasicGroup;
+import brooklyn.entity.basic.ConfigKeys;
 import brooklyn.entity.basic.MethodEffector;
 import brooklyn.entity.proxying.ImplementedBy;
 import brooklyn.entity.trait.HasShortName;
 import brooklyn.event.AttributeSensor;
+import brooklyn.event.basic.AttributeSensorAndConfigKey;
 import brooklyn.event.basic.Sensors;
 
 @ImplementedBy(WaratekApplicationGroupImpl.class)
@@ -30,13 +33,21 @@ public interface WaratekApplicationGroup extends BasicGroup, HasShortName {
     MethodEffector<Void> SET_ELASTIC_GROUP = new MethodEffector<Void>(WaratekApplicationGroup.class, "setElasticGroup");
     MethodEffector<Void> SET_ELASTIC_GROUP_HEAP = new MethodEffector<Void>(WaratekApplicationGroup.class, "setElasticGroupHeap");
 
+    AttributeSensorAndConfigKey<Long, Long> ELASTIC_MEMORY = ConfigKeys.newSensorAndConfigKey(Long.class,
+            "waratek.elasticGroup.heapMemory", "The heap memory allcoated to the elastic group", 0L);
+
     AttributeSensor<Integer> ELASTIC_GROUP_ID = Sensors.newIntegerSensor("waratek.elasticGroup.id", "The ID of the elastic group");
-    AttributeSensor<Long> ELASTIC_MEMORY = Sensors.newLongSensor("waratek.elasticGroup.heapMemory", "The heap memory allcoated to the elastic group");
+
+    AttributeSensor<Application> WARATEK_APPLICATION = Sensors.newSensor(Application.class,
+            "waratek.application.entity", "The parent application entity");
 
     @Effector(description="Set the elastic memory group for this applications JVCs")
-    void setElasticGroup(@EffectorParam(name="groupId") int groupId);
+    void setElasticGroup(@EffectorParam(name="groupId") Integer groupId);
 
     @Effector(description="Change the allocated heap memory for this elastic group")
-    void setElasticGroupHeap(@EffectorParam(name="heapSize") long heapSize);
+    void setElasticGroupHeap(@EffectorParam(name="heapSize") Long heapSize);
+
+    Integer getElasticGroup();
+    Long getElasticGroupHeap();
 
 }
--- src/main/java/brooklyn/entity/waratek/cloudvm/WaratekApplicationGroupImpl.java
+++ src/main/java/brooklyn/entity/waratek/cloudvm/WaratekApplicationGroupImpl.java
@@ -15,17 +15,58 @@
  */
 package brooklyn.entity.waratek.cloudvm;
 
+import java.util.concurrent.atomic.AtomicReference;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import brooklyn.entity.Application;
+import brooklyn.entity.Entity;
 import brooklyn.entity.basic.BasicGroupImpl;
+import brooklyn.entity.group.AbstractMembershipTrackingPolicy;
+import brooklyn.location.Location;
+import brooklyn.location.waratek.WaratekContainerLocation;
+import brooklyn.policy.Policy;
+import brooklyn.util.collections.MutableMap;
+
+import com.google.common.base.Optional;
+import com.google.common.base.Predicates;
+import com.google.common.collect.HashMultimap;
+import com.google.common.collect.Iterables;
+import com.google.common.collect.Multimap;
+import com.google.common.collect.Multimaps;
 
 public class WaratekApplicationGroupImpl extends BasicGroupImpl implements WaratekApplicationGroup {
 
     private static final Logger log = LoggerFactory.getLogger(WaratekApplicationGroupImpl.class);
 
+    private Policy policy;
+    private AtomicReference<Application> application = new AtomicReference();
+    private Multimap<JavaVirtualMachine, JavaVirtualContainer> layout = Multimaps.synchronizedMultimap(HashMultimap.<JavaVirtualMachine, JavaVirtualContainer>create());
+
+    @Override
     public void init() {
-        log.info("Created Waratek JVC Fabric");
+        log.info("Created Waratek Application Group");
+
+        // Track Application JVCs
+        policy = new AbstractMembershipTrackingPolicy(MutableMap.of("name", "Waratek Application Tracker")) {
+            @Override
+            protected void onEntityChange(Entity member) {
+                if (log.isDebugEnabled()) log.debug("Member {} updated for {} appliation", member, WaratekApplicationGroupImpl.this);
+                update();
+            }
+            @Override
+            protected void onEntityAdded(Entity member) {
+                if (log.isDebugEnabled()) log.debug("Member {} added to {} appliation", member, WaratekApplicationGroupImpl.this);
+                update();
+            }
+            @Override
+            protected void onEntityRemoved(Entity member) {
+                if (log.isDebugEnabled()) log.debug("Member {} removed from {} appliation", member, WaratekApplicationGroupImpl.this);
+                update();
+            }
+        };
+        addPolicy(policy);
     }
 
     @Override
@@ -33,14 +74,52 @@ public class WaratekApplicationGroupImpl extends BasicGroupImpl implements Warat
         return "ApplicationGroup";
     }
 
-    @Override
-    public void setElasticGroup(int groupId) {
-        setAttribute(ELASTIC_GROUP_ID, groupId);
+    public void update() {
+        synchronized (layout) {
+            layout.clear();
+            if (getMembers().isEmpty()) return;
+            for (Entity member : getMembers()) {
+                if (application.compareAndSet(null, member.getApplication())) {
+                    setAttribute(WARATEK_APPLICATION, application.get());
+                }
+                Optional<Location> found = Iterables.tryFind(member.getLocations(), Predicates.instanceOf(WaratekContainerLocation.class));
+                if (found.isPresent()) {
+                    WaratekContainerLocation container = (WaratekContainerLocation) found.get();
+                    JavaVirtualContainer jvc = container.getOwner();
+                    JavaVirtualMachine jvm = container.getJavaVirtualMachine();
+                    layout.put(jvm, jvc);
+                }
+            }
+        }
     }
 
     @Override
-    public void setElasticGroupHeap(long heapSize) {
-        setAttribute(ELASTIC_MEMORY, heapSize);
+    public void setElasticGroup(Integer groupId) {
+        setAttribute(ELASTIC_GROUP_ID, groupId);
+        for (JavaVirtualMachine jvm : layout.keySet()) {
+            // create the elastic group if not exists
+        }
+        for (JavaVirtualContainer jvc : layout.values()) {
+            // add it to the elstic group
+        }
     }
+
+    @Override
+    public Integer getElasticGroup() { return getAttribute(ELASTIC_GROUP_ID); }
+
+    @Override
+    public void setElasticGroupHeap(Long heapSize) {
+        setAttribute(ELASTIC_MEMORY, heapSize);
+        for (JavaVirtualMachine jvm : layout.keySet()) {
+            // create the elastic group if not exists
+            // set the elastic group size
+        }
+        for (JavaVirtualContainer jvc : layout.values()) {
+            // add it to the elstic group
+        }
+    }
+
+    @Override
+    public Long getElasticGroupHeap() { return getAttribute(ELASTIC_MEMORY); }
 
 }
